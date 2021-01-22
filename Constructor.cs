ee30b716391fd2e00179dae4c18d1a283752c14c
UnityDo <63395346+UnityDo@users.noreply.github.com>
2021-01-22 17:38:56 +0100

    Add files via upload

---
+++ Constructor.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Constructor : EditorWindow
+{
+  
+    int elementos;
+    GameObject objetoPrefab;
+    float offx, offy, offz;
+    public enum direccion { frente,atras,derecha,izquierda,arriba,abajo}
+    public direccion direccionConstruye;
+    
+    [MenuItem("Window/Constructor")]
+    static void Init()
+    {
+        Constructor ventana = (Constructor)EditorWindow.GetWindow(typeof(Constructor));
+        ventana.Show();
+    }
+    private void OnGUI() { 
+
+
+
+
+        direccionConstruye = (direccion)EditorGUILayout.EnumPopup("Primitive to create:", direccionConstruye);
+
+        offx = EditorGUILayout.FloatField("Off de x", offx);
+        offy = EditorGUILayout.FloatField("Off de y", offy);
+        offz = EditorGUILayout.FloatField("Off de z", offz);
+
+        elementos = EditorGUILayout.IntField("Numero de elemetos", elementos);
+
+        objetoPrefab = (GameObject)EditorGUILayout.ObjectField(objetoPrefab, typeof(GameObject), true, GUILayout.Width(200));
+        if (GUILayout.Button("Crea"))
+        {
+          
+            for (int i = 0; i < elementos; i++)
+            {
+                CreaPorDireccion(i);
+
+
+            }
+            //Instantiate(objetoPrefab);
+
+        }
+    }
+    void CreaPorDireccion(int veces)
+    {
+        GameObject Clon = new GameObject();
+        switch ((direccionConstruye))
+        {
+            case direccion.frente:
+                Clon = PrefabUtility.InstantiatePrefab(objetoPrefab) as GameObject;
+                Clon.transform.position = Vector3.forward * (Clon.GetComponentInChildren<MeshRenderer>().bounds.size.x+offx)*veces;
+                break;
+            case direccion.atras:
+                Clon = PrefabUtility.InstantiatePrefab(objetoPrefab) as GameObject;
+                Clon.transform.position = Vector3.back;
+                break;
+            case direccion.derecha:
+                Clon = PrefabUtility.InstantiatePrefab(objetoPrefab) as GameObject;
+                Clon.transform.position = Vector3.right;
+                break;
+            case direccion.izquierda:
+                Clon = PrefabUtility.InstantiatePrefab(objetoPrefab) as GameObject;
+                Clon.transform.position = Vector3.left;
+                break;
+            case direccion.arriba:
+                Clon = PrefabUtility.InstantiatePrefab(objetoPrefab) as GameObject;
+                Clon.transform.position = Vector3.up;
+                break;
+            case direccion.abajo:
+                Clon = PrefabUtility.InstantiatePrefab(objetoPrefab) as GameObject;
+                Clon.transform.position = Vector3.down;
+                break;
+        }
+       
+    }
+}
---
+++ Mueve_Editor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Mueve_Editor : Editor {
+	//Gira cuando se pulsa la g
+	[MenuItem ("Modifica/Rota45 %g")]
+	static void  Rotate45() {
+		var obj = Selection.activeGameObject;
+		obj.transform.Rotate(Vector3.up*45);
+	}
+	
+	[MenuItem ("Modifica/Mueve")]
+	static void  Mueve_Diez() {
+		var obj = Selection.activeGameObject;
+		obj.transform.position = new Vector3 (obj.transform.position.x + 10, obj.transform.position.y, obj.transform.position.z);
+	}
+	[MenuItem ("Modifica/Duplica")]
+	static void  Duplica() {
+		Object prefabRoot = PrefabUtility.GetPrefabParent (Selection.activeGameObject);
+
+		if (prefabRoot != null)
+			PrefabUtility.InstantiatePrefab (prefabRoot);
+		else
+			Instantiate (Selection.activeGameObject);
+	}
+
+}
